c52113f7308ad6eb446f29affcf4dbea8b340b36
Henry Bugajski <henry.bug.dev@gmail.com>
2025-03-02 18:08:39 -0500

    fix: add path to pages

--- src/payload/migrations/index.ts
+++ src/payload/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250221_201328 from './20250221_201328';
 import * as migration_20250302_161150 from './20250302_161150';
+import * as migration_20250302_230800 from './20250302_230800';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250302_161150.up,
     down: migration_20250302_161150.down,
-    name: '20250302_161150'
+    name: '20250302_161150',
+  },
+  {
+    up: migration_20250302_230800.up,
+    down: migration_20250302_230800.down,
+    name: '20250302_230800'
   },
 ];
